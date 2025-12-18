307d6f020e4efd1dd636d709e2165101353568d5
nayisntaround87 <naylasalwahayati@gmail.com>
2025-12-18 20:41:06 +0700

    menambah fungsi dan benerin typo di home view model

--- app/src/main/java/com/example/localapi/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/localapi/viewmodel/HomeViewModel.kt
@@ -1,2 +1,40 @@
 package com.example.localapi.viewmodel
 
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.localapi.modeldata.DataSiswa
+import com.example.localapi.repositori.RepositoryDataSiswa
+import kotlinx.coroutines.launch
+import okio.IOException
+import retrofit2.HttpException
+
+sealed interface StatusUiSiswa {
+    data class Success(val siswa: List<DataSiswa> = listOf()) : StatusUiSiswa
+    object Error : StatusUiSiswa
+    object Loading : StatusUiSiswa
+}
+class HomeViewModel(private val repositoryDataSiswa: RepositoryDataSiswa):
+        ViewModel() {
+    var listSiswa: StatusUiSiswa by mutableStateOf(StatusUiSiswa.Loading)
+        private set
+
+    init {
+        loadSiswa()
+    }
+
+    fun loadSiswa() {
+        viewModelScope.launch {
+            listSiswa = StatusUiSiswa.Loading
+            listSiswa = try {
+                StatusUiSiswa.Success(repositoryDataSiswa.getDataSiswa())
+            }catch (e: IOException){
+                StatusUiSiswa.Error
+            }
+            catch (e: HttpException){
+                StatusUiSiswa.Error
+            }
+        }
+    }
+
+}
