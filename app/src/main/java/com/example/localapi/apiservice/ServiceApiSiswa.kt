91647005c1ed40d3716c25e3b2a155fd7742aa88
nayisntaround87 <naylasalwahayati@gmail.com>
2025-12-18 17:52:40 +0700

    membuat ServiceApiSiswa.kt

---
+++ app/src/main/java/com/example/localapi/apiservice/ServiceApiSiswa.kt
@@ -0,0 +1,19 @@
+package com.example.localapi.apiservice
+
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+
+interface ServiceApiSiswa {
+    @GET("bacaTeman.php")
+    suspend fun getSiswa(): List<DataSiswa>
+
+    @POST("insertTM.php")
+    suspend fun postSiswa(@Body dataSiswa: DataSiswa):retrofit2.Response<Void>
+}
+
+//    @GET("baca1Teman.php/{id}")
+//    suspend fun getStasusSiswa(@Query("id") id: Int): DataSiswa
+
+//    @PUT("editTM.php/{id}")
+//    suspend fun editStatusSiswa(@Qyery("id") id: Int): DataSiswa
