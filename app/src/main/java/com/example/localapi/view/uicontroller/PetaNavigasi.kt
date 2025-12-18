71c135c993b75ff2d6f1b4ff9fdc219365ee1414
nayisntaround87 <naylasalwahayati@gmail.com>
2025-12-18 18:44:18 +0700

    menambahkan fungsi EntrySiswaScreen

--- app/src/main/java/com/example/localapi/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/localapi/view/uicontroller/PetaNavigasi.kt
@@ -1,2 +1,36 @@
 package com.example.localapi.view.uicontroller
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.localapi.view.route.DestinasiEntry
+import com.example.localapi.view.route.DestinasiHome
+
+@Composable
+fun DataSiswaApp(navController: NavHostController = rememberNavController(),
+    modifier: Modifier){
+    HostNavigasi(navController = navController)
+}
+
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+){
+    NavHost(navController = navController, startDestination = DestinasiHome.route,
+        modifier = Modifier ){
+        composable(DestinasiHome.route) {
+            HomeScreen(navigateToItemEntry = { navController.navigate
+                (DestinasiEntry.route)},
+                navigateToItemUpdate = {
+                    // navController.navigate("${DestinasiDetail.route}/${it}")
+                })
+        }
+        composable(DestinasiEntry.route){
+            EntrySiswaScreen(navigateBack = {navController.navigate(DestinasiHome.route) })
+        }
+    }
+}
