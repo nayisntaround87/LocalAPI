b2b6b89e504c8fd1835ec391bf74ce12591b48e2
nayisntaround87 <naylasalwahayati@gmail.com>
2025-12-18 17:51:33 +0700

    initial commit

---
+++ app/src/main/java/com/example/localapi/repositori/RepositoryDataSiswa.kt
@@ -0,0 +1,23 @@
+package com.example.localapi.repositori
+
+import com.example.localapi.apiservice.ServiceApiSiswa
+import com.example.localapi.modeldata.DataSiswa
+
+interface RepositoryDataSiswa {
+    suspend fun getDataSiswa() : List<DataSiswa>
+
+    suspend fun postDataSiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void>
+// suspend fun getSatuSiswa(id:Int) : DataSiswa
+// suspend fun editSatuSiswa(id:Int,dataSiswa: DataSiswa) retrofit2.Response<Void>
+// suspend fun hapusSatuSiswa(id:Int):retrofit2.Response<Void>
+}
+
+class JaringanRepositoryDataSiswa(
+    private val serviceApiSiswa: ServiceApiSiswa
+):RepositoryDataSiswa{
+    override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
+    override suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+// override suspend fun getSatuSiswa(id: Int): DataSiswa = serviceApiSiswa.getSatuSiswa(id)
+// override suspend fun
+// override suspend fun
+}
